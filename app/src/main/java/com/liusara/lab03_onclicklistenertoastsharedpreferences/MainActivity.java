e299dda8f85a32ffd0ffab71ca5f75a2599773ce
1444611 <sarayaoliu@gmail.com>
2020-02-19 14:15:57 -0500

    2/19/20

--- app/src/main/java/com/liusara/lab03_onclicklistenertoastsharedpreferences/MainActivity.java
+++ app/src/main/java/com/liusara/lab03_onclicklistenertoastsharedpreferences/MainActivity.java
@@ -2,6 +2,8 @@ package com.liusara.lab03_onclicklistenertoastsharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,15 +14,60 @@ public class MainActivity extends AppCompatActivity {
     TextView displayText2;
     TextView displayText3;
     TextView displayText4;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    int count1;
+    int count2;
+    int count3;
+    int count4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        displayText1 = findViewById(R.id.textBox1);
+        displayText2 = findViewById(R.id.textBox2);
+        displayText3 = findViewById(R.id.textBox3);
+        displayText4 = findViewById(R.id.textBox4);
+
+        count1 = sharedPreferences.getInt("count1", 0);
+        count2 = sharedPreferences.getInt("count2", 0);
+        count3 = sharedPreferences.getInt("count3", 0);
+        count4 = sharedPreferences.getInt("count4", 0);
+
         View.OnClickListener displayText = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast =  Toast.makeText(getApplicationContext(),"hi mom"+v.getId()+":"+v.getTag(), Toast.LENGTH_SHORT);
+                TextView textBox = (TextView) v;
+                int count;
+                if(textBox.getId() == R.id.textBox1)
+                {
+                    count1++;
+                    count = count1;
+                    editor.putInt("count1", count1);
+                }
+                else if(textBox.getId() == R.id.textBox2)
+                {
+                    count2++;
+                    count = count2;
+                    editor.putInt("count2", count2);
+                }
+                else if(textBox.getId() == R.id.textBox3)
+                {
+                    count3++;
+                    count = count3;
+                    editor.putInt("count3", count3);
+                }
+                else
+                {
+                    count4++;
+                    count = count4;
+                    editor.putInt("count4", count4);
+                }
+                editor.apply();
+                Toast toast =  Toast.makeText(getApplicationContext(),v.getId()+" was pressed "+ count + " times", Toast.LENGTH_SHORT);
                 toast.show();
             }
         };
